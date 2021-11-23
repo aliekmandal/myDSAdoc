29a617299ab91574cf957f219096afbb41e70342
aliekmandal <aliekmandal24@gmail.com>
2021-11-23 21:21:07 +0530

    Bits manupulation questions

---
+++ bits/trailingZeros.cpp.cpp
@@ -0,0 +1,12 @@
+// Question Link
+// https://www.interviewbit.com/problems/trailing-zeroes/
+
+//Given interger NUMS
+
+//Step 1: Define a count
+
+//Step 2:Check whether NUMS>0n using if
+
+//Step 3:Inside if run a while loop till (NUMS & 1)==0
+
+//Step 4: Return count
