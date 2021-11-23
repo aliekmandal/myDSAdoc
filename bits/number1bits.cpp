a4384188e31e05295a99a365fc33991e488a5d77
aliekmandal <aliekmandal24@gmail.com>
2021-11-23 21:29:21 +0530

    more bits question

---
+++ bits/number1bits.cpp
@@ -0,0 +1,12 @@
+//https://leetcode.com/problems/number-of-1-bits
+
+//define a count
+
+
+// Step 1: Run a while loop (n>0)
+
+// Step 2: A = A & (A-1)
+
+// Step 3 : increament the count
+
+//Step 4 :  return count
---
+++ bits/singleNumber.cpp
@@ -0,0 +1,6 @@
+//https://leetcode.com/problems/single-number
+
+//Approach
+//iterate through all elemnts in vector array
+//A ^=A --------> this line of code will find one time occuring element
+//return A
